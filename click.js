5b23ae695ace5aac94968e5de62008456ddd00c8
Michael <michael.scott.lyons@gmail.com>
2022-03-09 21:45:42 -0500

    Create click.js

---
+++ click.js
@@ -0,0 +1,11 @@
+s = 1, p= s => document.querySelector(s)
+setInterval(() => {
+  if (x=(s&&p('.bigBrotherDialog'))) {
+    x.style.visibility = "hidden";
+    s = 0;
+    setTimeout(() => {
+      p('.ui-button').click(s=1);
+      console.log("✓ "+new Date().toLocaleTimeString());
+    }, 1e3 + ((new Date)%1e4))
+  }
+}, 100)
