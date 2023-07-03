aafa4ea2058be96d9069d5afb570292813b6e798
Michael <michael.scott.lyons@gmail.com>
2023-07-03 10:22:30 -0400

    Update click.js

--- click.js
+++ click.js
@@ -28,4 +28,4 @@ setInterval(() => {
 }, 100)
 
 // short
-s=1,p=e=>document.querySelector(e),setInterval(()=>{(x=s&&p(".bigBrotherDialog"))&&(x.style.visibility="hidden",s=0,setTimeout(()=>{p(".ui-button").click(s=1),console.log("✓ "+(new Date).toLocaleTimeString())},1e3+9e3*Math.random()))},100);
+s=1,p=e=>document.querySelector(e),setInterval(()=>{(x=s&&p(".bigBrotherDialog"))&&(x.style.visibility="hidden",s=0,setTimeout(()=>{p(".ui-button").click(s=1),console.log("✓")},1e3+9e3*Math.random()))},100);
